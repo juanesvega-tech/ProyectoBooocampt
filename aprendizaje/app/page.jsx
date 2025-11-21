0fbadaf2f4bca02afd645d7eaf5168bc18f5c8ee
Luu190 <juanesvega19@gmail.com>
2025-11-21 09:01:11 -0500

    proyecto ya creado

---
+++ aprendizaje/app/page.jsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/login");
+  }, [router]);
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-black">
+      hola
+    </div>
+  );
+}
